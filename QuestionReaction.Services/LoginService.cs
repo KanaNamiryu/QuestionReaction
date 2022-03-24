7acfa1692677e4b2618c2f1d1da0c5dea0e724f6
Lucas FAEHN <lucas.faehn@outlook.fr>
2022-03-24 14:25:18 +0100

    Ajout HashPassword, update LoginAsync, update du champ password de la BDD

--- QuestionReaction.Services/LoginService.cs
+++ QuestionReaction.Services/LoginService.cs
@@ -8,52 +8,77 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Security.Claims;
 using Microsoft.AspNetCore.Authentication;
+using System.Security.Cryptography;
+using QuestionReaction.Data;
 
 namespace QuestionReaction.Services
 {
     public class LoginService : ILoginService
     {
         private readonly HttpContext _httpContext;
+        private readonly AppDbContext _ctx;
 
-        public LoginService(IHttpContextAccessor contextAccessor)
+        public LoginService(IHttpContextAccessor contextAccessor, AppDbContext ctx)
         {
             _httpContext = contextAccessor.HttpContext;
+            _ctx = ctx;
         }
 
         public async Task<bool> LoginAsync(string login, string password, bool rememberMe)
         {
             // check du login + mdp
-
-            // liens avec la bdd
-            var user = new User
+            var u = _ctx.Users.SingleOrDefault(u => u.Login == login);
+            if (u != null)
             {
-                Id = 1,
-                Name = "Lucas F",
-                Login = "lucas",
-                Password = "azerty"
-            };
+                var hash = HashPassword(password);
+                if (u.Password == hash)
+                {
+                    // enregistrer les infos sous forme de claim
+                    var claims = new List<Claim>
+                    {
+                        new Claim("name", u.Name),
+                        new Claim("id", u.Id.ToString())
+                    };
 
-            // enregistrer les infos sous forme de claim
-            var claims = new List<Claim>
-            {
-                new Claim("login", user.Login),
-                new Claim("name", user.Name),
-                new Claim("id", user.Id.ToString())
-            };
+                    // mettre la liste de claim dans une identity, puis dans un ClaimsPrincipal qui servira a se connecter
+                    var identity = new ClaimsIdentity(claims, "Cookies");
+                    var principal = new ClaimsPrincipal(identity);
+                    await _httpContext.SignInAsync(principal, new AuthenticationProperties
+                    {
+                        IsPersistent = rememberMe
+                    });
+                    return true;
+                }
+            }
 
-            // mettre la liste de claim dans une identity, puis dans un ClaimsPrincipal qui servira a se connecter
-            var identity = new ClaimsIdentity(claims, "Cookies");
-            var principal = new ClaimsPrincipal(identity);
-            await _httpContext.SignInAsync(principal, new AuthenticationProperties
-            {
-                IsPersistent = rememberMe
-            });
-            return true;
+            return false;
         }
 
         public async Task LogoutAsync()
         {
             await _httpContext.SignOutAsync();
         }
+
+        private string HashPassword(string clearPwd)
+        {
+            if (string.IsNullOrEmpty(clearPwd))
+                return string.Empty;
+
+            byte[] tmpSource;
+            byte[] tmpHash;
+
+            tmpSource = Encoding.ASCII.GetBytes(clearPwd);
+            using (var sha = SHA256.Create())
+            {
+                tmpHash = sha.ComputeHash(tmpSource);
+                StringBuilder sb = new StringBuilder();
+                for (int i = 0; i < tmpHash.Length; i++)
+                {
+                    sb.Append(tmpHash[i].ToString("X2"));
+                }
+                return sb.ToString();
+            }
+        }
+
     }
 }
