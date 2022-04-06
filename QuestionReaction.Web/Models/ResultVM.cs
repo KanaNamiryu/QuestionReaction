68ed6c5dc9ace3ba7934688f15c1c447a2da019a
Lucas FAEHN <lucas.faehn@outlook.fr>
2022-04-06 17:08:29 +0200

    ajout de la page et de la VM Result, implémentation de l'action dans le controller, début de la mise en page

---
+++ QuestionReaction.Web/Models/ResultVM.cs
@@ -0,0 +1,13 @@
+﻿using QuestionReaction.Data.Model;
+using System.Collections.Generic;
+
+namespace QuestionReaction.Web.Models
+{
+    public class ResultVM
+    {
+        public Question Question { get; set; }
+        public List<Choice> SortedChoices { get; set; }
+        public int VoteNumber { get; set; }
+
+    }
+}
