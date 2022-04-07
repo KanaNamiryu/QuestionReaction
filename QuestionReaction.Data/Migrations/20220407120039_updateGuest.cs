f62392189e944c46a09496987446997e1ed95def
Lucas FAEHN <lucas.faehn@outlook.fr>
2022-04-07 14:04:03 +0200

    ajout de la date dans le Model guest, ajout de migration et update database en conséquence

---
+++ QuestionReaction.Data/Migrations/20220407120039_updateGuest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace QuestionReaction.Data.Migrations
+{
+    public partial class updateGuest : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Date",
+                table: "Guests",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Date",
+                table: "Guests");
+        }
+    }
+}
